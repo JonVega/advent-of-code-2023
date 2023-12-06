9715beba07d1ca3a5548a796a0ac52182a9b0954
Jonathan Vega <jon.e.vega@icloud.com>
2023-12-07 00:46:38 +0900

    remove game # from each line in string

--- day2/c++/main.cpp
+++ day2/c++/main.cpp
@@ -6,12 +6,17 @@ using std::cout, std::endl, std::string, std::ifstream;
 
 int main() {
     string textFileString;
-    
+    unsigned int gameCounter = 1;
     ifstream textFile("../mini_input.txt");
 
     // Use a while loop together with the getline() function to read the file line by line
     while (getline (textFile, textFileString)) {
+        
+        textFileString = textFileString.substr(textFileString.find_first_of(":") + 2);
+        
         cout << textFileString << endl;
+
+        gameCounter++;
     }
 
     // Close the file
